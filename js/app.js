e64d5049a846378b4405bd26ec641de74b31a440
Bert Witzel <bwitzel82@gmail.com>
2021-05-22 19:12:06 -0400

    add close modal button and alert message on empty form

--- js/app.js
+++ js/app.js
@@ -4,7 +4,6 @@ $('.review').fadeIn(2000).show();
 // hide modal on page load
 $('#reviewModal').hide();
 
-
 // Create an animation for the paragraph with the id of "flashMessage." Chain an animation that 
 $('#flashMessage')
   .slideDown(1000)
@@ -20,17 +19,26 @@ $('#reviewButton').click(function(){
 $('#submitBtn').click(function(){
   const name = $('#restaurantNameInput').val();
   const review = $('#restaurantReviewInput').val();
-  const $div = $('<div class="review"></div>');
-  $div.css('display', 'block');
-  const $h3 = $('<h3></h3>');
-  $h3.text(name);
-  const $p = $('<p></p>');
-  $p.text(review);
-  $('#newRestaurant').append($div);
-  $($div).append($h3);
-  $($div).append($p);
-  $('input').val('');
-  $('textarea').val('');
+  if (name === '') {
+    alert('Please enter a Restaurant Name and Review');
+  } else {
+    const $div = $('<div class="review"></div>');
+    $div.css('display', 'block');
+    const $h3 = $('<h3></h3>');
+    $h3.text(name);
+    const $p = $('<p></p>');
+    $p.text(review);
+    $('#newRestaurant').append($div);
+    $($div).append($h3);
+    $($div).append($p);
+    $('input').val('');
+    $('textarea').val('');
+    $('#reviewModal').hide();
+  }
+});
+
+// Click handler to close modal
+$('#closeButton').click(function(){
   $('#reviewModal').hide();
 });
 
