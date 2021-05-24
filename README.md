7d06ccca8acee162a8d4aeba9696e70f21c5ef71
Bert Witzel <bwitzel82@gmail.com>
2021-05-24 13:08:20 -0400

    finishing touches
    add photo gallery, validate html, css and jQuery/JS

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # restaurant-review-app
- app using jQuery
+ Resturant Review app using jQuery
+
+ In this app I used jQuery to create all of the functionality of the app. This is a Restaurant review app where a user can click a button to add a review. This opens a modal window where there is the option to add the restaurant name and a short review. There is also the option to give the restaurant a star rating. This information is then used to create a new review on the main page. 
+
+ I have also created a small photo gallery with the FancyBox jQuery plugin. 
--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
+// initialize fancybox
+$('[data-fancybox="gallery"]').fancybox({
+	// Options will go here
+});
+
 // show first review with animation
 $('.review').fadeIn(2000).show();
 
@@ -7,32 +12,65 @@ $('#reviewModal').hide();
 // Create an animation for the paragraph with the id of "flashMessage." Chain an animation that 
 $('#flashMessage')
   .slideDown(1000)
-  .delay(2000)
-  .slideUp(1000)
+  .delay(3000)
+  .slideUp(1000);
 
 // reveal modal on button click
 $('#reviewButton').click(function(){
   $('#reviewModal').show();
 });
 
+// Helper function to create star
+function createStar(container) {
+  const $span = $('<span><span>');
+  const $star = $('<i></i>');
+  $star.addClass('fas fa-star');
+  $($span).append($star);
+  $(container).append($span);
+}
+
 // Click handler to create a working restaurant review form.
-$('#submitBtn').click(function(){
+$('#submitBtn').on('click', function(event){
   const name = $('#restaurantNameInput').val();
   const review = $('#restaurantReviewInput').val();
   if (name === '') {
+    event.preventDefault();
     alert('Please enter a Restaurant Name and Review');
   } else {
-    const $div = $('<div class="review"></div>');
+    const $div = $('<div></div>');
+    $div.addClass('review'); 
     $div.css('display', 'block');
     const $h3 = $('<h3></h3>');
     $h3.text(name);
-    const $p = $('<p></p>');
-    $p.text(review);
+    const $pReview = $('<p></p>');
+    $pReview.text(review);
     $('#newRestaurant').append($div);
     $($div).append($h3);
-    $($div).append($p);
+    $($div).append($pReview);
+    // see which star is checked then create star for the main page
+    const $checkedId = $(':checked').attr('id');
+    if ($checkedId === 'st1') {
+      for (let i = 0; i < 5; i++){
+        createStar($div);
+      } 
+    } else if ($checkedId === 'st2'){
+      for (let i = 0; i < 4; i++){
+        createStar($div);
+      } 
+    } else if ($checkedId === 'st3'){
+      for (let i = 0; i < 3; i++){
+        createStar($div);
+      } 
+    } else if ($checkedId === 'st4'){
+      for (let i = 0; i < 2; i++){
+        createStar($div);
+      } 
+    } else if ($checkedId === 'st5'){
+        createStar($div); 
+    }
     $('input').val('');
     $('textarea').val('');
+    $('input:radio:checked').prop('checked', false);
     $('#reviewModal').hide();
   }
 });
@@ -41,5 +79,4 @@ $('#submitBtn').click(function(){
 $('#closeButton').click(function(){
   $('#reviewModal').hide();
 });
-
 
